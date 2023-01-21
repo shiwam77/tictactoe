50d5977393495ff7af0bceee2c08638023af40d7
shiwamSliceit <shiwam.karn@gmail.com>
2023-01-21 14:48:55 +0530

    pr test

--- lib/constant/app_colors.dart
+++ lib/constant/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color white =  Color(0xFFFFFFFF);
   static const Color primaryColor = Color(0xFF5567C9);
   static const Color textColor =  Color(0xFF424242);
+  ///hello test
 }
